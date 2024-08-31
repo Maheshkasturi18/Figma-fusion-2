53d22b1ad0f24a88a9370b670974393e74ba95d9
MaheshWorkverse <kmahesh18001@gmail.com>
2024-09-01 01:39:14 +0530

    new-changes

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const passwordInput = document.getElementById("passwordInput");
+  const showIcon = document.getElementById("showIcon");
+  const hideIcon = document.getElementById("hideIcon");
 
+  showIcon.addEventListener("click", function () {
+    passwordInput.type = "text";
+    showIcon.style.display = "none";
+    hideIcon.style.display = "inline";
+  });
+
+  hideIcon.addEventListener("click", function () {
+    passwordInput.type = "password";
+    hideIcon.style.display = "none";
+    showIcon.style.display = "inline";
+  });
+});
